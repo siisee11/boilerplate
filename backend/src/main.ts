14e664cd1874296ff15a603cfa12351acbe17c77
Jay Nam <siisee111@gmail.com>
2024-08-23 19:33:41 +0900

    feat: setup opentelemetry

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
+import { config } from 'dotenv';
+config(); // load env before loading tracer and logger
+
+import otelSDK from './tracer'; // otelSDK should be imported before any other imports
+import createLogger from './logger';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+process.on('SIGTERM', () => {
+  otelSDK
+    .shutdown()
+    .then(
+      () => console.log('SDK shut down successfully'),
+      (err) => console.log('Error shutting down SDK', err),
+    )
+    .finally(() => process.exit(0));
+});
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  otelSDK.start();
+  const app = await NestFactory.create(AppModule, {
+    logger: createLogger(),
+  });
   await app.listen(3000);
 }
 bootstrap();
